5895503b52d396727aab93a1a7c810d281aa15ca
litmee <1970432@qq.com>
2022-08-17 10:35:44 +0800

    Hameln.go update

--- cmd/Hameln.go
+++ cmd/Hameln.go
@@ -1,7 +1,23 @@
 package main
 
-import "Hameln/pkg/cache"
+import (
+	"Hameln/pkg/cache"
+	"fmt"
+)
+
+type IsName string
+
+type User struct {
+	Id   int32
+	Name IsName
+}
+
+func (u *User) Apply() string {
+	return string(u.Name)
+}
 
 func main() {
+	user := User{Id: 1, Name: "Tom"}
+	fmt.Println(user.Apply())
 	cache.DoRedis()
 }
