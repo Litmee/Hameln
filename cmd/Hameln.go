4750c63f001a8ecb0f7f3bfaa339bf5c1213f754
litmee <1970432@qq.com>
2022-08-19 17:41:59 +0800

    Hameln.go update

--- cmd/Hameln.go
+++ cmd/Hameln.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"Hameln/pkg/cache"
 	"fmt"
 	"unsafe"
 )
@@ -17,5 +16,4 @@ func main() {
 	fmt.Printf("最大内存对齐边界: %v \n", align)
 	var user User
 	fmt.Printf("内存大小: %v \n", unsafe.Sizeof(user))
-	cache.DoRedis()
 }
